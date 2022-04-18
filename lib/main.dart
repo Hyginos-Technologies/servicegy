15876e58bf611b7e85857bc90a6f1fe140671e62
gimnakatugampala <gimnakatugampala1@gmail.com>
2022-04-19 00:04:19 +0530

    offline screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:servicegy/pages/auth/otp.dart';
 import 'package:servicegy/pages/other/offline.dart';
 
 void main() {
-  runApp(MaterialApp(initialRoute: '/login', routes: {
+  runApp(MaterialApp(initialRoute: '/offline', routes: {
     '/login': (context) => Login(),
     '/otp': (context) => OTP(),
     '/offline': (context) => offline(),
--- lib/pages/other/offline.dart
+++ lib/pages/other/offline.dart
@@ -12,7 +12,31 @@ class _offlineState extends State<offline> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Text('Offline Screen'),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Image.asset('assets/offline.png'),
+          SizedBox(height: 5.0),
+          Center(
+              child: Text(
+            'You’re Offline',
+            style: TextStyle(
+                fontFamily: 'MulishRegular',
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600),
+          )),
+          SizedBox(height: 5.0),
+          Container(
+            width: 300.0,
+            padding: const EdgeInsets.all(8.0),
+            margin: EdgeInsets.fromLTRB(30.0, 0.0, 0.0, 0.0),
+            child: Text(
+                'No internet or slow connection found. Check your connection or Try again',
+                style: TextStyle(
+                    fontFamily: 'MulishRegular',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w300,
+                    color: Colors.grey[600])),
+          )
+        ]),
       ),
     );
   }
